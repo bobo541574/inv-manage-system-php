4f278993ccd7a957714c242692d3eb34247bd9b1
bobo541574 <bobo541574.dev@gmail.com>
2020-09-22 10:42:22 +0630

    database_config

--- public_html/database/constants.php
+++ public_html/database/constants.php
@@ -2,9 +2,16 @@
 
 session_start();
 
-DEFINE('HOST', 'localhost');
-DEFINE('USER', 'root');
-DEFINE('PASS', '');
-DEFINE('DB', 'php_inventory');
+// DEFINE('HOST', 'localhost');
+// DEFINE('USER', 'root');
+// DEFINE('PASS', '');
+// DEFINE('DB', 'php_inventory');
 
-DEFINE("DOMAIN", "http://localhost/projects/PHP/Inventory-Management-System/public_html");
+// DEFINE("DOMAIN", "http://localhost/projects/PHP/Inventory-Management-System/public_html");
+
+DEFINE('HOST', 'ec2-54-157-234-29.compute-1.amazonaws.com');
+DEFINE('USER', 'tqbihjhzbymjwy');
+DEFINE('PASS', 'a1f536e9e268c10508f90bd851e6fc164e99dc692dcab82cf9c54e8162785fa8');
+DEFINE('DB', 'd7poa3ar9m70o6');
+
+DEFINE("DOMAIN", "https://inv-manage-system-php.herokuapp.com/");
