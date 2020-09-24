816e0bdc34914680e712902af6c4587219c965de
bobo541574 <bobo541574.dev@gmail.com>
2020-09-24 10:57:15 +0630

    update-db

--- public_html/database/constants.php
+++ public_html/database/constants.php
@@ -16,9 +16,9 @@ session_start();
 
 // DEFINE("DOMAIN", "http://localhost/projects/PHP/Inventory-Management-System/public_html");
 
-DEFINE('HOST', 'ec2-54-157-234-29.compute-1.amazonaws.com');
-DEFINE('USER', 'tqbihjhzbymjwy');
-DEFINE('PASS', 'a1f536e9e268c10508f90bd851e6fc164e99dc692dcab82cf9c54e8162785fa8');
-DEFINE('DB', 'd7poa3ar9m70o6');
+DEFINE('HOST', 'ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+DEFINE('USER', 'tyop9ksacdey5nlv');
+DEFINE('PASS', 'ji58h3d3yi4cbmku');
+DEFINE('DB', 'c7l85dgm4iev5tjq');
 
 DEFINE("DOMAIN", "https://inv-manage-system-php.herokuapp.com");
